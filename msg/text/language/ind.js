dde04cae4ed87731a5338f49646191df33f1f28a
n.slszt ナスワサルサ <80197467+nslszt@users.noreply.github.com>
2021-03-07 23:41:53 +0700

    Create ind.js

---
+++ msg/text/language/ind.js
@@ -0,0 +1,21 @@
+//Reading Text
+exports.menu = (prefix, pushname, botName, ownerName) => {
+    return`
+◩ *User Info*
+  │
+  ├❏ Nama : ${pushname}
+  └❏ 
+◩ *Bot Info*
+  │
+  ├❏ Bot Name : ${botName}
+  └❏ Owner Name : ${ownerName}
+◩ *About*
+  │
+  ├❏
+  └❏
+◩ *Menu*
+  │
+  ├❏
+  └❏
+`
+}
